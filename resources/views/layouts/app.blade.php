4067b916a8e376073bebfc1c7e309149fa824efa
Takeo <urkn99343@me.pikara.ne.jp>
2024-07-29 23:15:32 +0900

    app.bladeの修正

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -19,7 +19,7 @@
     <!-- font-awesome adn -->
      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css">
     <!-- Scripts -->
-    <!-- @vite(['resources/sass/app.scss', 'resources/js/app.js']) -->
+    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
     <style>
         .text-muted{
             display: none;
